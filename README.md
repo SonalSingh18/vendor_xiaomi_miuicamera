7aaf825aeaaf64d5cb4861f905808b8de95ab685
Pranav Vashi <neobuddy89@gmail.com>
2021-01-11 15:55:48 +0000

    MiuiCamera: Init MIUI 12 Camera

---
+++ README.md
@@ -0,0 +1,10 @@
+# MiuiCamera
+## Getting Started :
+### Cloning :
+- Clone this repo in vendor/xiaomi/miuicamera in your working directory by :
+```
+git clone https://github.com/SonalSingh18/vendor_xiaomi_miuicamera.git -b <branch-name> vendor/xiaomi/miuicamera
+```
+### Changes Required :
+- You will need [these changes in your device tree.](https://github.com/ArrowOS-Devices/android_device_xiaomi_sm6250-common/commit/a07b5bae7bb70ea819a89dffe4b63531ac0e07a6)
+- Done, continue building your ROM as you do normally.
---
+++ config.mk
@@ -0,0 +1,12 @@
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/xiaomi/miuicamera
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/etc,$(TARGET_COPY_OUT_SYSTEM)/etc) \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/framework,$(TARGET_COPY_OUT_SYSTEM)/framework) \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/lib64,$(TARGET_COPY_OUT_SYSTEM)/lib64) \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/vendor/camera,$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/camera)
+
+PRODUCT_PACKAGES += \
+    MiuiCamera \
+    miuires
