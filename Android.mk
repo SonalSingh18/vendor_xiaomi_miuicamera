26a936f8f00b9bda2deecf96cfef5159ae80f0bc
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-01-15 14:36:08 +0530

    MiuiCamera: Import missing lib for document scanner

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter xiaomi,$(BOARD_VENDOR)),)
 
-CAM_LIBS := libclone_c++_shared.so libcom.xiaomi.camera.algojni.so libcom.xiaomi.camera.mianodejni.so libcom.xiaomi.camera.requestutil.so libDocumentProcess_qcom.so libfenshen_snpe.so libffmpeg.so libjni_jpegutil_xiaomi.so libmulti-wakeup-engine.so librecord_video.so libvvc++_shared.so
+CAM_LIBS := libclone_c++_shared.so libcom.xiaomi.camera.algojni.so libcom.xiaomi.camera.mianodejni.so libcom.xiaomi.camera.requestutil.so libDocumentProcess_qcom.so libfenshen_snpe.so libffmpeg.so libjni_jpegutil_xiaomi.so libmulti-wakeup-engine.so librecord_video.so libvvc++_shared.so libDocumentProcess.so
 CAM_SYMLINKS := $(addprefix $(PRODUCT_OUT)/system/priv-app/MiuiCamera/lib/arm64/,$(notdir $(CAM_LIBS)))
 $(CAM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Cam lib link: $@"
