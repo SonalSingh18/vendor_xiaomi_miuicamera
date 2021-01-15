67a999954d91bbb0066e504f87c5a731fe92419f
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-01-15 13:52:12 +0530

    MiuiCamera: Fix 64MP and portrait on surya/karna/phoenix
     * took libcameraservicee from phoenix R, added dependency and shim for missing symbol

--- config.mk
+++ config.mk
@@ -4,6 +4,7 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/etc,$(TARGET_COPY_OUT_SYSTEM)/etc) \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/framework,$(TARGET_COPY_OUT_SYSTEM)/framework) \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/lib,$(TARGET_COPY_OUT_SYSTEM)/lib) \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/lib64,$(TARGET_COPY_OUT_SYSTEM)/lib64) \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/vendor/camera,$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/camera)
 
