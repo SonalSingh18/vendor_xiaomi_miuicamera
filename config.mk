d092bdc59797c91b58e56815b4e16529322d80cc
SonalSingh18 <sonal.singh.19993@gmail.com>
2021-01-29 12:49:39 +0530

    miatoll: MiuiCamera: Add a script to copy features and cheatcodes to sdcard
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- config.mk
+++ config.mk
@@ -2,6 +2,7 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/xiaomi/miuicamera
 
 PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/bin,$(TARGET_COPY_OUT_SYSTEM)/bin) \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/etc,$(TARGET_COPY_OUT_SYSTEM)/etc) \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/framework,$(TARGET_COPY_OUT_SYSTEM)/framework) \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/lib,$(TARGET_COPY_OUT_SYSTEM)/lib) \
