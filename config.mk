cd11dc3d40547f20bdc108baa442db5af866be8e
SonalSingh18 <sonal.singh.19993@gmail.com>
2021-01-31 03:24:12 +0530

    miatoll: MiuiCamera: Build MiuiExtraPhoto and MiuiScanner
    * Also add missing libmiuinative.so, else scanner app crashes
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- config.mk
+++ config.mk
@@ -7,10 +7,13 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/lib,$(TARGET_COPY_OUT_SYSTEM)/lib) \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/lib64,$(TARGET_COPY_OUT_SYSTEM)/lib64) \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/vendor/lib64,$(TARGET_COPY_OUT_VENDOR)/lib64) \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/priv-app/MiuiScanner/lib,$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiScanner/lib) \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/vendor/camera,$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/camera)
 
 PRODUCT_PACKAGES += \
     MiuiCamera \
+    MiuiExtraPhoto \
+    MiuiScanner \
     miuires
 
 PRODUCT_PRODUCT_PROPERTIES += \
